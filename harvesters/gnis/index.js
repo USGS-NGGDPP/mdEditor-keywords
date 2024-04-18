59313976543b90d02585e5bdebceb2879a390f04
Johnathan Aspinwall <jwaspin@gmail.com>
2024-04-18 14:42:15 -0600

    chore: Update package.json scripts and add GNIS Harvesters module

--- harvesters/gnis/index.js
+++ harvesters/gnis/index.js
@@ -1,1 +1,37 @@
-console.log("GNIS Harvesters");
+const axios = require('axios');
+const fs = require('fs');
+
+// const getProducts = async () => {
+//   try {
+//     const res = await axios.get(
+//       'https://tnmaccess.nationalmap.gov/api/v1/products'
+//     );
+//     console.log(res.data.messages);
+//     return [res.data.items, res.data.total];
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+const getDatasets = async () => {
+  try {
+    const res = await axios.get(
+      'https://tnmaccess.nationalmap.gov/api/v1/datasets?code=gnis'
+    );
+    return res.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const main = async () => {
+  // const [products, totalItems] = await getProducts();
+  // console.log('Products:', totalItems);
+  // fs.writeFileSync('temp/products.json', JSON.stringify(products, null, 2));
+
+  const datasets = await getDatasets();
+  console.log('Datasets:', datasets.length);
+  fs.writeFileSync('temp/datasets.json', JSON.stringify(datasets, null, 2));
+};
+
+main();
