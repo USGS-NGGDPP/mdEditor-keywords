610f9fb23263ca9238be9c24a5557e6a0823941a
Johnathan Aspinwall <jwaspin@gmail.com>
2024-05-30 15:27:59 -0600

    refactor: Remove parentId key if null in gcmd.js

--- harvesters/gcmd/gcmd.js
+++ harvesters/gcmd/gcmd.js
@@ -63,10 +63,6 @@ async function buildChildren(metadata) {
     });
     const narrowerChildren = await buildChildren(narrowerMetadata);
     const node = generateNode(narrowerMetadata);
-    // if node.parentId is null remove the key from the object
-    if (!node.parentId) {
-      delete node.parentId;
-    }
     node.children = narrowerChildren;
     children.push(node);
   }
@@ -75,6 +71,10 @@ async function buildChildren(metadata) {
 
 const buildKeywordTree = async metadata => {
   const node = generateNode(metadata);
+  // if node.parentId is null remove the key from the object
+  if (node.parentId === null) {
+    delete node.parentId;
+  }
   node.children = await buildChildren(metadata);
   return [node];
 };
