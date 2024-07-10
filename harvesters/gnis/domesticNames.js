9525b5f0d8204893ac5e962a14e0f0d9d3d1d6d4
Johnathan Aspinwall <jwaspin@gmail.com>
2024-07-09 18:33:59 -0600

    feat: Working on Domestic Names

--- harvesters/gnis/domesticNames.js
+++ harvesters/gnis/domesticNames.js
@@ -1,7 +1,64 @@
-import { GNIS_HARVESTERS_ENUM } from './gnisConfig';
+import { writeToLocalFile } from '../utils';
+import { DomesticNamesConfig, GNIS_HARVESTERS_ENUM } from './gnisConfig';
+import { parseTxtFileToJson } from './gnisUtils';
 
 const { DOMESTIC_NAMES } = GNIS_HARVESTERS_ENUM;
+const { NATIONAL_FILENAME } = DomesticNamesConfig;
 
 export async function harvestDomesticNames() {
-  console.log('Harvesting Domestic Names', DOMESTIC_NAMES);
+  console.log('Harvesting Domestic Names');
+  const jsonData = await parseTxtFileToJson(
+    `${DOMESTIC_NAMES}/${NATIONAL_FILENAME}`
+  );
+
+  const statesSet = new Set(jsonData.map(item => item.state_name));
+  const states = Array.from(statesSet);
+
+  const namesByState = {};
+  states.forEach(state => {
+    const stateData = jsonData.filter(item => item.state_name === state);
+    const uniqueNames = Array.from(
+      new Set(stateData.map(item => item.feature_name))
+    );
+
+    const uniqueData = uniqueNames.map(feature_name => {
+      const matchingItems = jsonData.filter(
+        item => item.feature_name === feature_name
+      );
+      return {
+        feature_name,
+        state_names: Array.from(
+          new Set(matchingItems.map(item => item.state_name))
+        ),
+        feature_ids: matchingItems.map(item => item.feature_id),
+        feature_class: matchingItems[0].feature_class,
+        county_names: Array.from(
+          new Set(matchingItems.map(item => item.county_name))
+        )
+      };
+    });
+
+    namesByState[state] = uniqueData;
+  });
+
+  const numberOfRecordsByState = {};
+  states.forEach(state => {
+    const count = namesByState[state].length;
+    numberOfRecordsByState[state] = count;
+  });
+
+  writeToLocalFile(numberOfRecordsByState, 'tmp/numberOfRecordsByState.json');
+
+  states.forEach(state => {
+    let filename = state;
+    if (filename === '') {
+      filename = 'unknown';
+    }
+    writeToLocalFile(
+      namesByState[state].sort((a, b) =>
+        a.feature_name.localeCompare(b.feature_name)
+      ),
+      `tmp/${filename}.json`
+    );
+  });
 }
--- harvesters/gnis/gnisConfig.js
+++ harvesters/gnis/gnisConfig.js
@@ -1,3 +1,5 @@
+// These are named the same as their directories,
+// so they can be used to dynamically load the config data
 export const GNIS_HARVESTERS_ENUM = {
   ANTARCTICA: 'Antarctica',
   DOMESTIC_NAMES: 'DomesticNames',
@@ -21,11 +23,14 @@ export const AntarcticaConfig = {
 export const DomesticNamesConfig = {
   NATIONAL_CITATION_FILANAME: 'DomesticNames_National_Text.xml',
   NATIONAL_FILENAME: 'DomesticNames_National.txt',
+  NATIONAL_DST_FILENAME: 'gnis-domestic-names.json',
   STATES_CITATION_FILENAME: 'DomesticNames_AllStates_Text.xml',
   STATES_DIRECTORY: 'States',
   STATES_FILENAME_PREFIX: 'DomesticNames_'
 };
 
+// These are named the same as their directories,
+// so they can be used to dynamically load the config data
 export const TOPICAL_ENUM = {
   GOVERNMENT_UNITS: 'GovernmentUnits',
   HISTORICAL_FEATURES: 'HistoricalFeatures',
@@ -33,16 +38,22 @@ export const TOPICAL_ENUM = {
 };
 
 export const TopicalConfig = {
+  // Must match the value of the TOPICAL_ENUM
   GovernmentUnits: {
     CITATION_FILENAME: 'GovernmentUnits_National_Text.xml',
-    FILENAME: 'GovernmentUnits_National.txt'
+    FILENAME: 'GovernmentUnits_National.txt',
+    DST_FILENAME: 'gnis-government-units.json'
   },
+  // Must match the value of the TOPICAL_ENUM
   HistoricalFeatures: {
     CITATION_FILENAME: 'HistoricalFeatures_National_Text.xml',
-    FILENAME: 'HistoricalFeatures_National.txt'
+    FILENAME: 'HistoricalFeatures_National.txt',
+    DST_FILENAME: 'gnis-historical-features.json'
   },
+  // Must match the value of the TOPICAL_ENUM
   PopulatedPlaces: {
     CITATION_FILENAME: 'PopulatedPlaces_National_Text.xml',
-    FILENAME: 'PopulatedPlaces_National.txt'
+    FILENAME: 'PopulatedPlaces_National.txt',
+    DST_FILENAME: 'gnis-populated-places.json'
   }
 };
